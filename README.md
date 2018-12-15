4939adba90939edaddcc3fb69c5fcd1a63e7267f
tmullen <tmullen1991@gmail.com>
2018-12-15 11:34:52 -0700

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TrainAcivity
+
+# Overview
+    The TrainActivity is a webpage that uses firebase to store user input and turn it into a train schedule using MomentJs to calculate when the next train is coming in real time.
+
+# How it works
+    The UI on the main HTML page prompts the user to input the 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         messagingSenderId: "644921315880"
     };
     firebase.initializeApp(config);
+    
     var database = firebase.database();
     var trainName = "";
     var destination = "";
@@ -28,26 +29,42 @@ $(document).ready(function () {
             firstTime: firstTime,
         });
     });
-    database.ref().on("child_added", function (childSnapshot) {
-        currentTime = moment().utc()
-        var firstTime = childSnapshot.val().firstTime
-        var hourDifference = currentTime.local().format('HH') - firstTime.slice(0, 2)
-        var minuteDifference = currentTime.local().format('mm') - firstTime.slice(3, 5)
-        var totalDifference = 60 * hourDifference + minuteDifference
-        var minutesAway = childSnapshot.val().frequency - totalDifference % childSnapshot.val().frequency
-        var nextArrival = currentTime.add(minutesAway, "minutes")
-        var displayNextArrival = nextArrival.format("hh:mm A")
-        var nextDate = nextArrival.format("MM-DD-YYYY")
+    var updateTime = function () {
+        database.ref().on("child_added", function (childSnapshot) {
+            currentTime = moment().utc()
+            var firstTime = childSnapshot.val().firstTime
+            var hourDifference = currentTime.local().format('HH') - firstTime.slice(0, 2)
+            var minuteDifference = currentTime.local().format('mm') - firstTime.slice(3, 5)
+            var totalDifference = 60 * hourDifference + minuteDifference
+            var minutesAway = childSnapshot.val().frequency - totalDifference % childSnapshot.val().frequency
+            var nextArrival = currentTime.add(minutesAway, "minutes")
+            var displayNextArrival = nextArrival.format("hh:mm A")
+            var nextDate = nextArrival.format("MM-DD-YYYY")
+            var name = childSnapshot.val().trainName
+            $("#data-goes-here").append("<tr class='train-info' key='" + childSnapshot.key + "'> " +
+                "<th class='" + name + "'>" + name + "</th>" +
+                "<td class='" + name + "'>" + childSnapshot.val().destination + "</td>" +
+                "<td class='" + name + "'>" + childSnapshot.val().frequency + "</td>" +
+                "<td class='" + name + "'>" + displayNextArrival + "</td>" +
+                "<td class='" + name + "'>" + minutesAway + "</td>" +
+                "<td class='" + name + "'>" + nextDate + "</td>" +
+                "</tr>")
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
+    $(document).on("mouseenter", ".train-info", function () {
+        $(this).append("<a class='remove btn-floating btn-small waves-effect waves-light blue right''><i  class=' material-icons'>remove_circle_outline</i></a>")
+        var current = this
+        var currentDb = database.ref($(this).attr("key"))
+        $(".remove").on("click", function () {
+            currentDb.remove()
+            $(current).remove()
+        })
+    }).on("mouseleave", ".train-info", function () {
+        $(".remove").remove()
+    })
 
-        $("#data-goes-here").append("<tr>" +
-            "<th class='train-name'>" + childSnapshot.val().trainName + "</th>" +
-            "<td class='destination'>" + childSnapshot.val().destination + "</td>" +
-            "<td class='frequency'>" + childSnapshot.val().frequency + "</td>" +
-            "<td class='next-arrival'>" + displayNextArrival + "</td>" +
-            "<td class='minutes-away'>" + minutesAway + "</td>" +
-            "<td class='next-date'>" + nextDate + "</td>" +
-            "</tr>")
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+    updateTime()
+    setInterval(function () { $(".train-info").remove(), updateTime(); }, 60000)
 });
