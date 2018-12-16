3c96e2afd1194cc366b9d7f209e3279251013177
tmullen <tmullen1991@gmail.com>
2018-12-15 17:18:01 -0700

    gave up on login:(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,26 +9,6 @@ $(document).ready(function () {
         messagingSenderId: "644921315880"
     };
     firebase.initializeApp(config);
-    // setup google authentication so the user must use this to view page
-    var provider = new firebase.auth.GoogleAuthProvider();
-    
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-
 
     // variables to pass and recieve infromatio from/to firebase in the click event and updateTime functions below
     var database = firebase.database();
